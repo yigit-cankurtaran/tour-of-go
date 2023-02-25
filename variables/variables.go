862cc67cd4f6ec4cd30af19bd78f6fe4996753dc
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-25 15:46:11 +0300

    Finished the variables exercise

---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+// by default, variables are initialized to the zero value
+// for example, an int is 0, a float is 0.0, a string is ""
+// a bool is false, a pointer is nil
