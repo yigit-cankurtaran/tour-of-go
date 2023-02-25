fc02a83d55593459068e0ee555752819fd2ae28a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-25 16:20:32 +0300

    Learned about type inference, played around with it

---
+++ types/type-inference.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// v := 42
+	// int
+	// v := "hi"
+	// string
+	// v := 10.0
+	// float64
+	v := true
+	// bool
+	// types can be inferred from the value
+	fmt.Printf("v is of type %T\n", v)
+}
